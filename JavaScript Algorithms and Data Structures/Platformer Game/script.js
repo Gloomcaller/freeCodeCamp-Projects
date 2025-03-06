9b5ce07bb0cdc715474381647d6bc02867a13240
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-03-06 19:01:01 +0100

    Enough for today, my homie needs me

--- JavaScript Algorithms and Data Structures/Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures/Platformer Game/script.js
@@ -81,6 +81,19 @@ const keys = {
     }
 };
 
+const movePlayer = (key, xVelocity, isPressed) => {
+    if (!isCheckpointCollisionDetectionActive) {
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        return;
+    }
+
+    switch (key) {
+        case "ArrowLeft":
+
+    }
+}
+
 const startGame = () => {
     canvas.style.display = "block";
     startScreen.style.display = "none";
