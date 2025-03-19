e856a06817d95d7dcff4a9e02328725b76988003
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-03-19 20:09:40 +0100

    Dice game final commit.

--- JavaScript Algorithms and Data Structures/Dice Game/script.js
+++ JavaScript Algorithms and Data Structures/Dice Game/script.js
@@ -22,7 +22,7 @@ const rollDice = () => {
     for (let i = 0; i < 5; i++) {
         const randomDice = Math.floor(Math.random() * 6) + 1;
         diceValuesArr.push(randomDice);
-    };
+    }
 
     listOfAllDice.forEach((dice, index) => {
         dice.textContent = diceValuesArr[index];
@@ -51,11 +51,7 @@ const getHighestDuplicates = (arr) => {
     const counts = {};
 
     for (const num of arr) {
-        if (counts[num]) {
-            counts[num]++;
-        } else {
-            counts[num] = 1;
-        }
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
     }
 
     let highestCount = 0;
@@ -79,8 +75,6 @@ const getHighestDuplicates = (arr) => {
     if (highestCount >= 3) {
         updateRadioOption(0, sumOfAllDice);
     }
-
-    updateRadioOption(5, 0);
 };
 
 const detectFullHouse = (arr) => {
@@ -96,8 +90,23 @@ const detectFullHouse = (arr) => {
     if (hasThreeOfAKind && hasPair) {
         updateRadioOption(2, 25);
     }
+};
 
-    updateRadioOption(5, 0);
+const checkForStraights = (arr) => {
+    const sortedNumbersArr = arr.sort((a, b) => a - b);
+    const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+    const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+    const smallStraightsArr = ["1234", "2345", "3456"];
+    const largeStraightsArr = ["12345", "23456"];
+
+    if (smallStraightsArr.some(straight => uniqueNumbersStr.includes(straight))) {
+        updateRadioOption(3, 30);
+    }
+
+    if (largeStraightsArr.includes(uniqueNumbersStr)) {
+        updateRadioOption(4, 40);
+    }
 };
 
 const resetRadioOptions = () => {
@@ -130,22 +139,6 @@ const resetGame = () => {
     resetRadioOptions();
 };
 
-const checkForStraights = (arr) => {
-    const uniqueSortedValues = [...new Set(arr)].sort((a, b) => a - b);
-    const uniqueValuesString = uniqueSortedValues.join("");
-
-    const smallStraights = ["1234", "2345", "3456"];
-    const largeStraights = ["12345", "23456"];
-
-    if (largeStraights.includes(uniqueValuesString)) {
-        updateRadioOption(4, 40);
-    } else if (smallStraights.some((straight) => uniqueValuesString.includes(straight))) {
-        updateRadioOption(3, 30);
-    }
-
-    updateRadioOption(5, 0);
-};
-
 rollDiceBtn.addEventListener("click", () => {
     if (rolls === 3) {
         alert("You have made three rolls this round. Please select a score.");
@@ -157,6 +150,7 @@ rollDiceBtn.addEventListener("click", () => {
         getHighestDuplicates(diceValuesArr);
         detectFullHouse(diceValuesArr);
         checkForStraights(diceValuesArr);
+        updateRadioOption(5, 0);
     }
 });
 
