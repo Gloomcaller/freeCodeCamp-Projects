c7574ca4ede0163b7ce954a34c4c9551a4494955
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-05-30 15:27:50 +0200

    ok

---
+++ JavaScript Algorithms and Data Structures/Trivia Bot/script.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Bot";
+let botLocation = "New Now";
+let favoriteLanguage = "HTML";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "There’s a web page for everything there is to know about " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "You can find all the information about " + favoriteLanguage +
+    " standards on a single page. The page contains everything from the history of HTML to the latest technical specifications of this markup language. ";
+console.log(codingFact);
+
+codingFact = "You can find this page about " + favoriteLanguage + " at this adress --> https://html.spec.whatwg.org/";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
