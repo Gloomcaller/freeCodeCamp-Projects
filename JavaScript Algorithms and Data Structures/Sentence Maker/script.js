e4c62f0f3f25baacf19ba6292038ba10dfda5e30
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-05-30 15:38:37 +0200

    Need to actually think of a story now.

---
+++ JavaScript Algorithms and Data Structures/Sentence Maker/script.js
@@ -0,0 +1,23 @@
+let adjective = "";
+let noun = "";
+let verb = "";
+let place = "";
+let adjective2 = "";
+let noun2 = "";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +
+    ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "";
+noun = "";
+verb = "";
+place = "";
+adjective2 = "";
+noun2 = "";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +
+    ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
