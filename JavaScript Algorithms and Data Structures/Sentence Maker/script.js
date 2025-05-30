db14868f044102b37139e1b0383b9524d8a0b95b
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-05-30 15:40:26 +0200

    Good enough tbh.

--- JavaScript Algorithms and Data Structures/Sentence Maker/script.js
+++ JavaScript Algorithms and Data Structures/Sentence Maker/script.js
@@ -1,21 +1,21 @@
-let adjective = "";
-let noun = "";
-let verb = "";
-let place = "";
-let adjective2 = "";
-let noun2 = "";
+let adjective = "gigantic";
+let noun = "dragon";
+let verb = "sneezing";
+let place = "magical cave";
+let adjective2 = "enormous";
+let noun2 = "marshmallows";
 
 let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +
     ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
 
 console.log("First story: " + firstStory);
 
-adjective = "";
-noun = "";
-verb = "";
-place = "";
-adjective2 = "";
-noun2 = "";
+adjective = "tiny";
+noun = "unicorn";
+verb = "dancing";
+place = "rainbow castle";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
 
 let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 +
     ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
